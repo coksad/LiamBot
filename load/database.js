6b80cdd6ecac0ddc45f92f8e3278737139d0c2de
coksad <62913612+coksad@users.noreply.github.com>
2021-07-31 12:59:01 +0100

    Update database.js

--- load/database.js
+++ load/database.js
@@ -12,7 +12,7 @@ new pg.Client({
     password: "8ae36ec0a4656b7889e9f3c343bce7d38d69be648d6b9f6bbf8edab5265e0269",
     database: "d8dhvclmvu2bn9",
     port: 5432,
-    host: "34.254.92.24",
+    host: "ec2-176-34-222-188.eu-west-1.compute.amazonaws.com",
     ssl: true
 }); 
 
