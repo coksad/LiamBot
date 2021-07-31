119c0d798bca4761f2f7a3107c7f505c81966b2c
coksad <62913612+coksad@users.noreply.github.com>
2021-07-31 12:41:31 +0100

    Update database.js

--- load/database.js
+++ load/database.js
@@ -1,3 +1,4 @@
+```js
 const { Client } = require('pg');
 
 /**
@@ -5,15 +6,25 @@ const { Client } = require('pg');
  * @requires pg
  */
 module.exports = {
-	id: 'database',
-	client: new Client({ connectionString: process.env.BOT_NAME_DB || process.env.DATABASE_URL }),
-	exec: async function () {
-		try {
-			await this.client.connect();
+    id: 'database',
+    client: 
+new pg.Client({
+    user: "ovfngxfznytitu",
+    password: "ovfngxfznytitu:8ae36ec0a4656b7889e9f3c343bce7d38d69be648d6b9f6bbf8edab5265e0269",
+    database: "d8dhvclmvu2bn9",
+    port: 5432,
+    host: "34.254.92.24",
+    ssl: true
+}); 
 
-			console.log('Connected database client.');
-		} catch (e) {
-			console.warn('Database failed to connect\n', e.stack);
-		}
-	}
-};
+Client({ connectionString: process.env.BOT_NAME_DB || process.env.DATABASE_URL }),
+    exec: async function () {
+        try {
+            await this.client.connect();
+
+            console.log('Connected database client.');
+        } catch (e) {
+            console.warn('Database failed to connect\n', e.stack);
+        }
+    }
+};```
