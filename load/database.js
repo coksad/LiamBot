13cc805eba9ed00ea2c96f92253af3e557c48579
coksad <62913612+coksad@users.noreply.github.com>
2021-12-12 16:12:08 +0000

    Update database.js

--- load/database.js
+++ load/database.js
@@ -8,8 +8,8 @@ module.exports = {
     id: 'database',
     client: 
 new Client({
-    user: "ovfngxfznytitu",
-    password: "8ae36ec0a4656b7889e9f3c343bce7d38d69be648d6b9f6bbf8edab5265e0269",
+    user: "cxulweweyzwkjy",
+    password: "ccab7a9bd43c6af650855563c04241a6d3433cd947ce5c680f9844feb03421",
     database: "d8dhvclmvu2bn9",
     port: 5432,
     host: "ec2-176-34-222-188.eu-west-1.compute.amazonaws.com",
