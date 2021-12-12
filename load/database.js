9cbeaebcea86a70773801bdbd6fe95cea2e331ef
Yikey <28969308+gt-c@users.noreply.github.com>
2021-12-12 14:04:00 -0500

    Update database.js

--- load/database.js
+++ load/database.js
@@ -6,15 +6,10 @@ const { Client } = require('pg');
  */
 module.exports = {
     id: 'database',
-    client: 
-new Client({
-    user: "ovfngxfznytitu",
-    password: "8ae36ec0a4656b7889e9f3c343bce7d38d69be648d6b9f6bbf8edab5265e0269",
-    database: "d8dhvclmvu2bn9",
-    port: 5432,
-    host: "ec2-176-34-222-188.eu-west-1.compute.amazonaws.com",
-    ssl: { rejectUnauthorized: false }
-        }),
+    client: new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }),
     exec: async function () {
         try {
             await this.client.connect();
@@ -22,6 +17,6 @@ new Client({
             console.log('Connected database client.');
         } catch (e) {
             console.warn('Database failed to connect\n', e.stack);
-}
-}
+        }
+    }
 }; 
